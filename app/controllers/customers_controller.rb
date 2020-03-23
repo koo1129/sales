09a68e6c2596a8738f1ceccf1a9abfb1e65abdf5
koo1129 <koo.1192icloud.com>
2020-03-23 20:50:00 +0900

    修正

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,13 @@
 class CustomersController < ApplicationController
     
     def new
+        @customers = Customer.new
+        #@customers.users << current_user
     end
-  
+    
     def create
+        binding.pry
     end
-
+    
 end
+    
